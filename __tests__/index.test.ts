7c3da16f53cc7ea33c6c467f62919a7c3dcd1836
dyh_a <xinzon32177@163.com>
2022-11-27 16:45:03 +0800

    test: base clear测试完成

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -2,6 +2,29 @@ import * as testTarget from '../src';
 
 describe('keymap', function () {
   test('base', () => {
-    expect(testTarget.test()).toBe('test');
+    const fn = jest.fn();
+    const km = new testTarget.Keymap([{ keys: 'Control+a', handler: fn }]);
+    expect(fn.mock.calls.length).toBe(0);
+
+    km.trigger('Control+a');
+    expect(fn.mock.calls.length).toBe(1);
+
+    km.trigger('Control+a');
+    km.trigger('Control+a');
+    expect(fn.mock.calls.length).toBe(3);
+  });
+  test('clear', () => {
+    const fn = jest.fn();
+    const km = new testTarget.Keymap([{ keys: 'Control+a', handler: fn }]);
+    expect(fn.mock.calls.length).toBe(0);
+
+    km.trigger('Control+a');
+    expect(fn.mock.calls.length).toBe(1);
+
+    km.clear();
+
+    km.trigger('Control+a');
+    km.trigger('Control+a');
+    expect(fn.mock.calls.length).toBe(1);
   });
 });
